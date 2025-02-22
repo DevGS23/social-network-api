58df2f06188961cda093f6e043c508bf642a3310
Mona Lisa <gabrielle.seri@outlook.com>
2025-02-21 23:21:56 -0500

    Initial project setup with server.js and dependencies

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,19 +7,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/socialNetworkDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+    .connect('mongodb://127.0.0.1:27017/socialNetworkDB')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Could not connect to MongoDB:', err));
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-app.get("/", (req, res) => {
-  res.send("Social Network API is running!");
+app.get('/', (req, res) => {
+    res.send('Social Network API is running!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
