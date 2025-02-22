3a753bc6524dfc027338e16715de82fe5a523c20
Mona Lisa <gabrielle.seri@outlook.com>
2025-02-22 14:50:14 -0500

    Added create and update methods to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -29,6 +29,46 @@ const thoughtController = {
             res.status(500).json(err);
         }
     },
+    // Create a thought
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+
+            // Add the thought to the user's thoughts array
+            await User.findByIdAndUpdate(
+                req.body.userId,
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+
+            res.json(thought);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // Update a thought
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findByIdAndUpdate(
+                req.params.thoughtId,
+                { $set: req.body },
+                { new: true, runValidators: true }
+            );
+
+            if (!thought) {
+                return res
+                    .status(404)
+                    .json({ message: 'No thought found with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
 };
 
 module.exports = thoughtController;
