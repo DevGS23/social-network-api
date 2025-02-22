c6270affb140fb1dffc59c57aab50a637aa75773
Mona Lisa <gabrielle.seri@outlook.com>
2025-02-22 14:43:05 -0500

    Added update user method to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,6 +58,27 @@ const userController = {
             res.status(500).json(err);
         }
     },
+    // Update a user
+    async updateUser(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                { $set: req.body },
+                { new: true, runValidators: true }
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: 'No user found with this id!' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
 };
 
 module.exports = userController;
